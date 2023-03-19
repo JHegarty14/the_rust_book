6c813e61b5a6bf3c41c7345b210d125a5fb1a0f2
Jack Hegarty <jmhegarty14@gmail.com>
2023-03-19 12:28:41 -0700

    chapter 14

---
+++ chapter14_3/adder/src/main.rs
@@ -0,0 +1,7 @@
+use add_one;
+use rand::{self, Rng};
+
+fn main() {
+    let num = rand::thread_rng().gen_range(0..=100);
+    println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+}
